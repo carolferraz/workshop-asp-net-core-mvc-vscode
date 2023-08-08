f757a858d75807736c1af64024ddeb006259fdb2
Carol Ferraz <fferraz.carol@gmail.com>
2023-08-08 16:03:28 -0300

    Add Create Form with Foreign key not null (referencial integrity)

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers;
 
 public class SellersController : Controller
 {
-    private readonly SellerService _sellerService;
+    private readonly SellerService _sellerService; //SellerService receive the data from the DB, in this case called SalesWebMvcContext. So this is a connecting (making a service) between View and DbContext.
 
     public SellersController(SellerService sellerService)
     {
@@ -13,11 +14,23 @@ public class SellersController : Controller
     }
 
 
-      public IActionResult Index()
+    public IActionResult Index()
     {
         var list = _sellerService.FindAll();
         return View(list);
     }
 
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Seller seller)
+    {
+       _sellerService.Insert(seller);
+       return RedirectToAction(nameof(Index));
+    }
 
 }
--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -4,7 +4,7 @@ namespace SalesWebMvc.Services;
 
 public class SellerService
 {
-    private readonly SalesWebMvcContext _context;
+    private readonly SalesWebMvcContext _context; //SalesWebMvcContext is our connection to DB by DbContext. So SellerService Get the information from DB and will be connected to Insert/Update/Delete data in DB by SalesWebMvcContext.
 
     public SellerService(SalesWebMvcContext context)
     {
@@ -14,6 +14,13 @@ public class SellerService
     public List<Seller> FindAll()
     {
         return _context.Seller.ToList();
+    }
+
+    public void Insert(Seller obj)
+    {
+        obj.Department = _context.Department.First();
+        _context.Add(obj);
+        _context.SaveChanges();
     }
 
 }
